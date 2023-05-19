87f7ec8d9fc780ff930eba2d5837d758a87b45da
seyun <81186461+ParkSeYun98@users.noreply.github.com>
2023-05-19 14:10:34 +0900

    video 진행중

---
+++ src/components/video/VideoCategory.vue
@@ -0,0 +1,78 @@
+<template>
+  <div>
+    <div class="first-buttons">
+      <button @click="clickFirstTap" value="국가 대표" class="btn btn-dark">
+        국가 대표
+      </button>
+      <button @click="clickFirstTap" value="해외 축구" class="btn btn-dark">
+        해외 축구
+      </button>
+      <button @click="clickFirstTap" value="축구 강의" class="btn btn-dark">
+        축구 강의
+      </button>
+    </div>
+
+    <div class="second-buttons">
+      <div v-if="firstTap === '국가 대표'">
+        <button @click="clickSecondTap" value="대한민국" class="btn btn-danger">
+          대한민국
+        </button>
+        <button @click="clickSecondTap" value="아르헨티나" class="btn btn-info">
+          아르헨티나
+        </button>
+      </div>
+
+      <div v-if="firstTap === '해외 축구'">
+        <button @click="clickSecondTap" value="맨시티" class="btn btn-info">
+          맨시티
+        </button>
+        <button @click="clickSecondTap" value="첼시" class="btn btn-primary">
+          첼시
+        </button>
+        <button @click="clickSecondTap" value="토트넘" class="btn">
+          토트넘
+        </button>
+      </div>
+
+      <div v-if="firstTap === '축구 강의'">
+        <button @click="clickSecondTap" value="개인기" class="btn btn-dark">
+          개인기
+        </button>
+        <button @click="clickSecondTap" value="슈팅" class="btn btn-dark">
+          슈팅
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "VideoCategory",
+  data() {
+    return {
+      firstTap: 0,
+      secondTap: 0
+    };
+  },
+  created() {
+    this.firstTap = 0;
+    this.secondTap = 0;
+  },
+  methods: {
+    clickFirstTap(event) {
+      this.firstTap = event.target.value;
+    },
+    clickSecondTap(event) {
+      this.secondTap = event.target.value;
+      this.$store.dispatch("searchYoutube", this.secondTap + "축구");
+    }
+  }
+};
+</script>
+
+<style scoped>
+.btn {
+  margin: 1px;
+}
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,49 @@
 import Vue from "vue";
 import Vuex from "vuex";
-// import axios from "axios";
+import axios from "axios";
 // import router from "../router";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    videos: [],
+    video: null
+  },
   getters: {},
-  mutations: {},
-  actions: {},
+  mutations: {
+    SEARCH_YOUTUBE(state, videos) {
+      state.videos = videos;
+    },
+    CLICK_VIDEO(state, video) {
+      state.video = video;
+    }
+  },
+  actions: {
+    searchYoutube({ commit }, keyword) {
+      const URL = "https://www.googleapis.com/youtube/v3/search";
+      const API_KEY = process.env.VUE_APP_YOUTUBE_API_KEY;
+      console.log(process.env.VUE_APP_YOUTUBE_API_KEY);
+
+      axios({
+        url: URL,
+        method: "GET",
+        params: {
+          key: API_KEY,
+          part: "snippet",
+          q: keyword,
+          type: "video",
+          maxResults: 10
+        }
+      })
+        .then(response => {
+          commit("SEARCH_YOUTUBE", response.data.items);
+        })
+        .catch(err => console.log(err));
+    },
+    clickVideo({ commit }, video) {
+      commit("CLICK_VIDEO", video);
+    }
+  },
   modules: {}
 });
